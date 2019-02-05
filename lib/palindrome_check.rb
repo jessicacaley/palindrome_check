3e62e0eff2e3389687cb2fb68f0d6e398f304463
shrutivanw <shruti.dalvi@gmail.com>
2019-02-05 11:42:44 -0500

    C11 updates - improved instructions
    Updates to clarify instructions for the assignment.

--- lib/palindrome_check.rb
+++ lib/palindrome_check.rb
@@ -1,5 +1,7 @@
 # A method to check if the input string is a palindrome.
 # Return true if the string is a palindrome. Return false otherwise.
+# Time complexity: ?
+# Space complexity: ?
 def palindrome_check(my_phrase)
   raise NotImplementedError
 end
